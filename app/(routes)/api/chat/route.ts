f663b09cc4346974afa5be3866cfd75191646dbc
Loo <loo@Mac-mini-M2.lan>
2023-09-01 23:51:52 +0800

    add firebase connection

--- app/(routes)/api/chat/route.ts
+++ app/(routes)/api/chat/route.ts
@@ -1,9 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { PaLMCall } from '../../PaLM/connect'
+import { getZephyrPoem } from '../../firebase/connect'
 
 export async function GET(request: NextRequest) {
-    const resultJson = await chat()
-    const response = NextResponse.json({ ...resultJson })
+    // const resultJson = await chat()
+    const poem = await getZephyrPoem()
+    const response = NextResponse.json({ poem })
     response.headers.set('Access-Control-Allow-Origin', '*')
     return response
 }
